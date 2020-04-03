b72d823dd3e35c7c81c226ad7cf1983ab367de7e
Réda <r.elmidaoui@outlook.fr>
2020-04-03 03:15:40 +0200

    ajout d'un écran de connexion et ajout d'un formulaire d'inscription

--- Developpement/inscription.php
+++ Developpement/inscription.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="bootstrap.css">    <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
 </head>
 <body>
 <?php
@@ -25,7 +25,7 @@ catch(Exception $e)
 
 
 ?>
-
+<!--
 <h3>Inscription</h3>
  
  
@@ -41,6 +41,48 @@ catch(Exception $e)
  Confirmer le mot de passe :	<INPUT TYPE="TEXT" SIZE="40" NAME="mdpverif"><br><br>
  <br><br>
  <input type="submit" name="envoi" value="Valider" />
+
+ -->
+
+ <div id="container">
+            <!-- zone de connexion -->
+            
+            <form action="verification.php" method="POST">
+                <h1>Inscription</h1>
+                
+                <label><b>Nom</b></label>
+                <input type="text" placeholder="Entrez votre nom" name="nom" required>
+
+                <label><b>Prénom</b></label>
+                <input type="text" placeholder="Entrez votre prénom" name="prenom" required>
+
+                <label><b>Nom de compte</b></label>
+                <input type="text" placeholder="Entrez votre nom de compte" name="login" required>
+
+                <label><b>Adresse e-mail</b></label>
+                <input type="text" placeholder="Entrez votre email" name="email" required>
+
+                <label><b>Confirmez votre adresse e-mail></label>
+                <input type="text" placeholder="Entrez votre email" name="emailverif" required>
+
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrez votre mot de passe" name="mdp" required>
+
+                <label><b>Confirmez votre mot de passe</b></label>
+                <input type="password" placeholder="Entrez votre mot de passe" name="mdpverif" required>
+
+                
+
+                <input type="submit" id='submit' value='LOGIN' >
+                <?php
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                }
+                ?>
+            </form>
+        </div>
 
 </body>
 </html>
